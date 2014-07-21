b73a21554dd33ba0c0cf31efc06907d98bb7b220
Jeferson Ferreira da Silva <jeferson.ferreira@ifpa.edu.br>
2014-07-21 20:51:45 -0300

    Revisão
    Estruturas de controle

--- index.php
+++ index.php
@@ -1,12 +1,29 @@
 <?php
+
 $nome = "anos";
 $idade = 2;
-echo $idade+$nome;
+echo $idade + $nome;
 
-if(true){
+if (true) {
     echo "verdade";
+} elseif (true) {
+    echo "Mentira";
 }
-elseif(true) {
-        echo "Mentira";
-   }
+$num = 10;
 
+echo $num>10?"Maior":"Menor";
+for($i=0;$i<10;$i++){
+    echo "$i";
+}
+$i=20;
+while ($i<10){
+    echo $i--;
+}
+do{
+    echo "Do";
+}while (FALSE);
+$dados[]=0;
+$dados = array(1,2,3,4);
+foreach ($dados as $valor){
+    echo $valor;
+}
